fd5436b88039b866121c84441a385dc92d299203
ianta <iantastream@gmail.com>
2023-09-12 00:55:24 -0700

    10

--- app/src/main/java/com/example/cocorounds/ui/theme/Color.kt
+++ app/src/main/java/com/example/cocorounds/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BrightOrange = Color(0xFFE73725)
+val CherryOrange = Color(0xFFDF1B3F)
+
+val NavyBlue = Color(0xFF19204E)
+val CoolBlack = Color(0xFF010000)
